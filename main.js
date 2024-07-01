c0708e608d9d4d8eeef8758cdaa4d95efce70986
Gu55t4v0 <137287797+Gu55t4v0@users.noreply.github.com>
2024-07-01 08:55:22 -0300

    Final Aula-4

---
+++ main.js
@@ -0,0 +1,19 @@
+const caixaPrincipal = document.querySelector(".Caixa-principal");
+const caixaPerguntas = document.querySelector(".Caixa-pergunta");
+const caixaAlternativas = document.querySelector(".Caixa-alternativas");
+const caixaResultado = document.querySelector(".Caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+    enunciado: "pergunta 1",
+    alternativas: ["altenativa 1",
+        "altenativa 2"]
+    },
+
+
+    {enunciado:"pergunta 2",
+        alternativas: ["alternativa 1",
+            "alternativas 2"]
+    },
+]
